b72d2191130b50ec5a9235c002879e30fc29a94e
Tomoya Suzuki <forever.blue.moon@gmail.com>
2019-10-01 16:38:57 +0900

    19章「前準備」完了。テストコードの重複を排除するテクニックとして、setUpに括りだす事ができることを学んだ。

--- xunit.py
+++ xunit.py
@@ -2,24 +2,34 @@ class TestCase:
     def __init__(self, name):
         self.name = name
     
+    def setUp(self):
+        pass
+    
     def run(self):
+        self.setUp()
         method = getattr(self,self.name)
         method()
 
 class WasRun(TestCase):
-    def __init__(self, name):
-        self.wasRun = None
-        super().__init__(name)
+    def setUp(self):
+        self.wasRun = False
+        self.wasSetUp = True
 
     def testMethod(self):
-        self.wasRun = 1
+        self.wasRun = True
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
