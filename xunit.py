78ac1cbe59bd2625c2b0d98307df7a5525094e36
Tomoya Suzuki <forever.blue.moon@gmail.com>
2019-10-02 19:30:11 +0900

    第21章「数え上げ」完了。章タイトルは実装内容ぽい。学ぶべきはテストを書く順番を重要視すべきということ。また、大きなテストを書いて失敗したなら、一度戻る事も検討すべきということ。

--- xunit.py
+++ xunit.py
@@ -9,14 +9,20 @@ class TestCase:
         pass
     
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self,self.name)
         method()
         self.tearDown()
+        return result
 
 class WasRun(TestCase):
     def setUp(self):
         self.log = 'setUp '
+
+    def testBrokenMethod(self):
+        raise Exception
 
     def testMethod(self):
         self.wasRun = True
@@ -25,11 +31,33 @@ class WasRun(TestCase):
     def tearDown(self):
         self.log = self.log + 'tearDown '
 
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+
+    def summary(self):
+        return '%d run, 0 faild' %self.runCount
+
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
         assert('setUp testMethod tearDown ' == test.log)
+    
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert('1 run, 0 faild' == result.summary())
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert('1 run, 1 failed' == result.summary())
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+# TestCaseTest("testFailedResult").run()
